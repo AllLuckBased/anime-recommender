4e3fc39e892d1fa2be33390d29b6fe5534d70495
Jud White <jud.white@blueskysystems.com>
2022-08-08 02:29:49 -0500

    feat: create db dir if it doesn't exist

--- database.js
+++ database.js
@@ -1,8 +1,13 @@
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
+import fs from "fs";
 
 export let myAniDB = null
 export async function connect() {
+    if (!fs.existsSync("./db")) {
+        fs.mkdirSync("./db")
+    }
+
     myAniDB = await open({
         filename: './db/globalAniDB.db',
         driver: sqlite3.Database
